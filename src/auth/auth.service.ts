61d1e6c264002d33e161132216435cab90e2b0ac
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-25 19:21:08 -0300

    Creates Auth Module with Guards and Strategies

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @Inject('UserService')
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) {}
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findOneByEmail(email);
+        if (user && user.password === password) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async login(user: any) {
+        const payload = { username: user.email, sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload)
+        };
+    }
+}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, Inject } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { jwtConstants } from "./constants";
+import { UserService } from "src/user/user.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @Inject('UserService')
+        private readonly userService: UserService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: jwtConstants.secret
+        })
+    }
+
+    async validate(payload: any) {
+        const user = await this.userService.findOneByEmail(payload.username);
+        console.log('USER', user);
+        return { id: payload.sub, email: payload.username, name: user.name, enterprise: user.enterprise }
+    }
+}
