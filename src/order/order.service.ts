72bff5500aeaf8e1d634920d663f550c82f0692a
mbernardes19 <bernardes.matheus@outlook.com>
2020-11-05 00:48:59 -0300

    Adds method to get an order by its number

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Order } from 'src/model/order.entity';
+import { differenceInMilliseconds } from 'date-fns';
 
 @Injectable()
 export class OrderService {
@@ -12,4 +13,32 @@ export class OrderService {
     async getOrdersFromEnterprise(enterpriseName: string): Promise<Order[]> {
         return await this.orderRepository.find({ order: { orderNumber: 'ASC'}, relations: ['orderedItems', 'enterprise'] , where: {enterprise: { name: enterpriseName }}})
     }
+
+    async getOrdersFromAllEnterprises(): Promise<Order[]> {
+        return await this.orderRepository.find({ order: { orderNumber: 'ASC'}, relations: ['orderedItems', 'enterprise'] })
+    }
+
+    async getOrderByNumber(orderNumber: string): Promise<Order> {
+        const order = await this.orderRepository.findOne({
+            where: {orderNumber},
+            relations: ['orderedItems', 'enterprise', 'orderedItems.item', 'orderedItems.prodServInfo']
+        }) 
+        const deliveryDate = this.getFarestDeliveryDate(order)
+        order.deliveryDate = deliveryDate;
+        console.log('ORDER', order);
+        return order;
+    }
+
+    private getFarestDeliveryDate(order: Order): Date {
+        let difference = 0;
+        let farestDate = new Date();
+        order.orderedItems.map(item => {
+            const diff = differenceInMilliseconds(item.deliveryDate, new Date())
+            if (diff > difference) {
+                difference = diff;
+                farestDate = item.deliveryDate;
+            }
+        })
+        return farestDate;
+      }
 }
